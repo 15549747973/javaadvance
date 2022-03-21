3d3b0f7a32f15c35672ff8236da1ecbef6b5759e
banzhanhu <banzhanhu@qq.com>
2022-03-21 16:23:22 +0800

    1.2

--- java/src/02/ClientSocket.java
+++ java/src/02/ClientSocket.java
@@ -1,3 +1,8 @@
+import javax.jnlp.FileContents;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 /**
  * @author 蔡心勇
  * @create 2022/3/20 - 13:59
@@ -5,5 +10,15 @@
 public class ClientSocket {
     public static void main(String[] args)throws Exception {
 
+        FileContents socket = null;
+        OutputStream outputStream=null;
+        PrintStream printStream=new PrintStream(outputStream);
+
+        while (true){
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("请输入");
+            printStream.println(scanner.nextLine());
+            printStream.flush();
+        }
     }
 }
